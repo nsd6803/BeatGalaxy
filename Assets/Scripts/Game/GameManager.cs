2f6e952769b44cc48b462d5fee087aa6682ce97a
MajestroM94 <123757131+MajestroM94@users.noreply.github.com>
2023-04-21 01:59:35 +0300

    Vers1
    Выбор башен из левого меню, появление башен, рандомное появление врагов

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    void Awake() { instance = this; }
+
+    public Spawner spawner;
+
+    void Start()
+    {
+
+
+        StartCoroutine(WaveStartDelay());
+    }
+
+    IEnumerator WaveStartDelay()
+    {
+        //Wait for X seconds
+        yield return new WaitForSeconds(2f);
+        //Start the enemy spawning
+        GetComponent<EnemySpawner>().StartSpawning();
+    }
+
+}
